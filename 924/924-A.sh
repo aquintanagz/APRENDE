ceae53b4982f566244fd976ab63c40c46246543c
Aaron Quintana <aquintanagz@gmail.com>
2020-09-24 23:49:50 -0500

    Todos los comandos que vimos en la clase

--- 924/924-A.sh
+++ 924/924-A.sh
@@ -1,3 +1,54 @@
 #! /bin/bash
 
 git init
+
+git add *
+
+git status
+
+git commit -m "Mi primer Commit"
+
+
+git remote add origin https://github.com/aquintanagz/APRENDE.git
+
+git push -u origin master
+
+------
+
+
+git clone https://github.com/aquintanagz/APRENDE.git
+
+-----  
+Crear rama para el programador 1
+
+git checkout -b aquintanagz master
+
+git pull origin master
+
+git add *
+
+git commit -m "trabaje sobre los xml"
+
+git push origin aquintanagz
+
+----
+bajar los cambios de la rama del progamador 1
+
+git checkout -b oscar origin/aquintanagz
+
+git pull origin aquintanagz
+
+git add *
+
+git commit -m "yo oscar trabaje las vistas"
+
+git push origin oscar
+
+
+
+
+
+
+
+
+
